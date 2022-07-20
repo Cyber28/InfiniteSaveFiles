17415e8062694d618d96504a060e17d28fd4e0c8
Cyber28 <cybergaming0628@gmail.com>
2022-07-20 16:51:00 +0200

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,80 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+namespace InfiniteSaveFilesNS
+{
+	[BepInPlugin("InfiniteSaveFiles", "InfiniteSaveFiles", "1.0.0")]
+	public class InfiniteSaveFiles : BaseUnityPlugin
+	{
+		public static BepInEx.Logging.ManualLogSource L;
+		private void Awake()
+		{
+			L = Logger;
+			Harmony.CreateAndPatchAll(typeof(Patches));
+		}
+	}
+
+	public class Patches
+	{
+		[HarmonyPatch(typeof(WorldManager), "GetAllSaves")]
+		[HarmonyPrefix]
+		public static bool WMGAS(ref List<SaveGame> __result)
+		{
+			var allSaves = new List<SaveGame>();
+			foreach (var file in new DirectoryInfo(Application.persistentDataPath).GetFiles())
+			{
+				if (file.Extension == ".sav")
+				{
+					InfiniteSaveFiles.L.LogInfo($"found save file: {file.Name} | id: {file.Name.Split('.')[0].Split('_')[1]}");
+					var json = File.ReadAllText(file.FullName);
+					var saveId = file.Name.Split('.')[0].Split('_')[1]; // please dont castigate me for this
+					if (!string.IsNullOrEmpty(json))
+						allSaves.Add(SaveGame.LoadFromString(json, saveId));
+				}
+			}
+			if (allSaves.Count == 0)
+				allSaves.Add(new SaveGame() { SaveId = "0" });
+			__result = allSaves;
+			return false;
+		}
+
+		[HarmonyPatch(typeof(SelectSaveScreen), "Start")]
+		[HarmonyPrefix]
+		public static bool SSSStart(SelectSaveScreen __instance)
+		{
+			__instance.BackButton.Clicked += (Action)(() => GameCanvas.instance.SetScreen(GameCanvas.instance.OptionsScreen));
+			List<SaveGame> allSaves = WorldManager.instance.GetAllSaves();
+			foreach (var save in allSaves)
+			{
+				CustomButton customButton = UnityEngine.Object.Instantiate<CustomButton>(PrefabManager.instance.ButtonPrefab);
+				customButton.transform.SetParent(__instance.ButtonsParent);
+				customButton.transform.localScale = Vector3.one;
+				customButton.transform.localPosition = Vector3.zero;
+				customButton.transform.localRotation = Quaternion.identity;
+				customButton.TextMeshPro.text = WorldManager.instance.GetSaveSummary(save);
+				customButton.Clicked += (() =>
+				{
+					WorldManager.instance.Save(save);
+					TransitionScreen.instance.StartTransition((Action)(() => WorldManager.RestartGame()));
+				});
+			}
+			CustomButton newSave = UnityEngine.Object.Instantiate<CustomButton>(PrefabManager.instance.ButtonPrefab);
+			newSave.transform.SetParent(__instance.ButtonsParent);
+			allSaves.Sort((SaveGame x, SaveGame y) => Int32.Parse(x.SaveId) - Int32.Parse(x.SaveId)); // WHY CANT YOU CHAIN THESEEE
+			var saveIndex = Int32.Parse(allSaves.Last().SaveId) + 1; // 0-indexed, so has to be incremented again for displaying
+			InfiniteSaveFiles.L.LogInfo($"last save id: {saveIndex}");
+			newSave.TextMeshPro.text = SokLoc.Translate("label_start_new_save", LocParam.Create("save_index", (saveIndex + 1).ToString()));
+			newSave.Clicked += (() =>
+			{
+				WorldManager.instance.Save(new SaveGame() { SaveId = saveIndex.ToString() });
+				TransitionScreen.instance.StartTransition((Action)(() => WorldManager.RestartGame()));
+			});
+			return false;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+![ISF logo](https://raw.githubusercontent.com/Cyber28/InfiniteSaveFiles/main/icon.png)
+
+# Infinite Save Files
+
+Are 3 save files not enough? How about infinite?
+
+## Features
+
+- Lets you create up to 2,147,483,648 save files
+
+## Installation (manual)
+
+You need the [pre-configured BepInEx pack](https://stacklands.thunderstore.io/package/BepInEx/BepInExPack_Stacklands) for this mod. If you installed BepInEx from a different place, you will need to edit the config files.
+
+Extract the contents of this folder to `Stacklands/BepInEx/plugins/InfiniteSaveFiles`. The final folder layout should look like this:
+```
+plugins/
+  InfiniteSaveFiles/
+    InfiniteSaveFiles.dll
+    icon.png
+    manifest.json
+    README.md
+```
+
+## Troubleshooting
+
+This mod affects your save files minimally, it only changes the file name and the save id, both of which are easy to modify manually if something goes wrong
+
+If you have any questions or feedback, join us in the developer-supported [Stacklands Modding Discord server](https://discord.gg/j3FjwZVyWh)
+
+This project was made with love for the Stacklands community 💖💜
